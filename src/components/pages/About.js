086ce3fe1d3c1fc09e28c79271039351ee1f8dd2
Your Name <daniela.kirilova@gmx.de>
2020-12-19 18:10:18 +0100

    about changed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,8 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
+      <h1>About the App</h1>
+      <p>Search Github users</p>
       <p>Version: 1.0.0</p>
     </Fragment>
   );
